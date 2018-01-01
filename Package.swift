abfff6672c1c6214bf662f3854925acc2f6dddfe
Andrew Barba <abarba.77@gmail.com>
2018-01-01 15:55:44 -0500

    crypto module

--- Package.swift
+++ Package.swift
@@ -6,13 +6,12 @@ let package = Package(
     name: "Lisk",
     dependencies: [
         .package(url: "https://github.com/AndrewBarba/Bluebird.swift.git", from: "2.0.1"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.8.0"),
         .package(url: "https://github.com/vzsg/ed25519.git", from: "0.2.0")
     ],
     targets: [
         .target(
             name: "Lisk",
-            dependencies: ["Bluebird", "CryptoSwift", "Ed25519"],
+            dependencies: ["Bluebird", "Ed25519"],
             path: "./Sources"
         ),
         .testTarget(
--- Sources/Crypto/Crypto.swift
+++ Sources/Crypto/Crypto.swift
@@ -6,31 +6,51 @@
 //
 
 import Foundation
-import CryptoSwift
 import Ed25519
 
 public struct Crypto {
 
+    /// Generate public and private keys from a given secret
     public func keys(fromSecret secret: String) throws -> (publicKey: String, privateKey: String) {
-        let bytes = secret.bytes.sha256()
+        let bytes = SHA256(secret).digest()
         let seed = try Seed(bytes: bytes)
         let keyPair = KeyPair(seed: seed)
-        return (keyPair.publicKey.bytes.toHexString(), keyPair.privateKey.bytes.toHexString())
+        let publicKey = keyPair.publicKey.bytes.hexString()
+        let privateKey = keyPair.privateKey.bytes.hexString()
+        return (publicKey, privateKey)
     }
 
+    /// Extract Lisk address from a public key
     public func address(fromPublicKey publicKey: String) -> String {
-
-        let bytes = publicKey.sha256().bytes
-
-        print(Array(bytes[0..<8]))
-
-        var _bytes: [UInt8] = []
-        for i in 0..<8 {
-            _bytes.append(bytes[7 - i])
-        }
-
-        let data = Data(bytes: _bytes)
-        let value = Int(littleEndian: data.withUnsafeBytes { $0.pointee })
+        let bytes = SHA256(publicKey.hexBytes).digest()[0..<8].reversed()
+        let data = Data(bytes: Array(bytes))
+        let value = Int(bigEndian: data.withUnsafeBytes { $0.pointee })
         return "\(value)L"
+    }
+}
+
+extension String {
+
+    fileprivate var hexBytes: [UInt8] {
+        var bytes = [UInt8]()
+        bytes.reserveCapacity(count/2)
+        var index = startIndex
+        for _ in 0..<count/2 {
+            let nextIndex = self.index(index, offsetBy: 2)
+            if let b = UInt8(self[index..<nextIndex], radix: 16) {
+                bytes.append(b)
+            } else {
+                return []
+            }
+            index = nextIndex
+        }
+        return bytes
+    }
+}
+
+extension Sequence where Self.Element == UInt8 {
+
+    fileprivate func hexString() -> String {
+        return map { String(format: "%02hhx", $0) }.joined()
     }
 }
